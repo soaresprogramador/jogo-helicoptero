25df6036deff64135e6f2fb9b60a8edaf80fbe3b
Paulo Soares <soaresprogramador@gmail.com>
2020-09-24 15:39:16 -0300

    Primeira colisao usando jquery 1.11.1

--- js/js.js
+++ js/js.js
@@ -42,6 +42,7 @@ function start() {
         moveInimigo1();
         moveInimigo2();
         moveAmigo();
+        colisao();
     }
 
     //movimento do fundo
@@ -134,6 +135,41 @@ function start() {
                 $("#disparo").remove();
                 podeAtirar = true;
             }
+        }
+    }
+
+    function colisao() {
+        var colisao1 = ($("#jogador").collision($("#inimigo1")));
+            
+        if (colisao1.length>0) {
+                
+            inimigo1X = parseInt($("#inimigo1").css("left"));
+            inimigo1Y = parseInt($("#inimigo1").css("top"));
+            explosao1(inimigo1X,inimigo1Y);
+        
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left",694);
+            $("#inimigo1").css("top",posicaoY);
+        }
+        
+    }
+
+    function explosao1(inimigo1X, inimigo1Y) {
+        
+        $("#fundoGame").append("<div id='explosao1'></div>");
+        $("#explosao1").css("background-image", "url(imgs/explosao.png");
+        var div = $("#explosao1");
+        div.css("top", inimigo1Y);
+        div.css("left", inimigo1X);
+        div.animate({ width:230, opacity: .2 }, "slow");
+
+        var tempoExplosao = window.setInterval(removeExplosao, 800);
+
+        function removeExplosao() {
+
+            div.remove();
+            window.clearInterval(tempoExplosao);
+            tempoExplosao = null;
         }
     }
     
